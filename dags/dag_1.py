17c30416f3911696299cdaea7463c2507b7fc093
Juli Tay <119788019+julitays@users.noreply.github.com>
2023-07-18 17:14:52 +0300

    Update dag_1.py

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -1,1 +1,25 @@
+from airflow import Dag
+from airflow.operators import PythonOperators, BashOperators 
+from datetime import datetime  
+import pathlib
+
+default_args = {
+  'owner' : 'julitays',
+  'depends_on_past': False, 
+  'start_date': datetime(2023, 18, 07),
+  'retries': 0
+}
+
+dag = DAG('python_hello_world_dag',
+          default_args = default_args,
+          cutchup = False,
+          schedule_interval = '00 20 * * *')
+
+def hello():
+  return print('hello, world')
+
+t1 = PythonOperator(
+  task_id = 'task_1',
+  python_callable = hello,
+  dag = dag )
 
